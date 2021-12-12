66f6cc935647b4f147d138ab6b4ca3defb2eecd2
Hamza Nouinou <nouinouhamza@gmail.com>
2021-12-12 17:09:03 +0100

    feat: add cors setup

--- app.js
+++ app.js
@@ -9,7 +9,19 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
+
+function setupCORS(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key');
+    res.header('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+}
+app.all('/*', setupCORS);
 
 app.use('/auth', authRoute);
 app.use('/user', userRoute);
